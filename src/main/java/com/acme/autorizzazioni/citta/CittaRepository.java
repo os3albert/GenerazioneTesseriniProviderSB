a1a768ed780cf4d330b6773af4c6e82aa1559be6
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-20 14:49:19 +0200

    reverted files

--- src/main/java/com/acme/autorizzazioni/citta/CittaRepository.java
+++ src/main/java/com/acme/autorizzazioni/citta/CittaRepository.java
@@ -9,20 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CittaRepository extends PagingAndSortingRepository<Citta, Long> {
 	
-	public List<Citta> findByNome(String nome);
-	
-	public List<Citta> findByIdBetween(long da, long a);
-	
-	public List<Citta> findByIdGreaterThan(long id);
-
-	public List<Citta> findByNomeStartingWithIgnoreCase(String nome);  // con ignoreCase ignori il fatto che inizi o meno con la lettera maiuscola
-	
-	public List<Citta> findByNomeEndingWithIgnoreCase(String nome); 
-
-    public List<Citta> findByNomeContainingIgnoreCase(String nome); 
-    
-    public List<Citta> findByNomeStartingWithIgnoreCaseAndCognomeContainingIgnoreCase(String nome, String cognome); 
-    
-    public List<Citta> findByNomeIsNotIgnoreCase(String nome); 
 
 }
