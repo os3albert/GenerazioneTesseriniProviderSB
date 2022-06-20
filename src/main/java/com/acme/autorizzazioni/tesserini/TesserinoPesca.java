a6f657abd3905b5a65cdd0877008d9a1aa76f87e
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-20 14:13:18 +0200

    update

--- src/main/java/com/acme/autorizzazioni/tesserini/TesserinoPesca.java
+++ src/main/java/com/acme/autorizzazioni/tesserini/TesserinoPesca.java
@@ -3,6 +3,7 @@ package com.acme.autorizzazioni.tesserini;
 import javax.persistence.Entity;
 
 import com.acme.autorizzazioni.licenze.Licenza;
+import com.acme.autorizzazioni.persone.Persona;
 
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +15,10 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 @Entity
 public class TesserinoPesca extends TesserinoAbstract {
-
+	
 	@Builder
-	public TesserinoPesca(long id, int annoValidita, Licenza licenza) {
-		super(id, annoValidita, licenza);
+	public TesserinoPesca(long id, int annoValidita, Licenza licenza, Persona tesserini_persona) {
+		super(id, annoValidita, licenza, tesserini_persona);
 		// TODO Auto-generated constructor stub
 	}
 
