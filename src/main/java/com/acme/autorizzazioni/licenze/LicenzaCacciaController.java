74e1b6933b79b50dec3929ce18086ba05e84b52f
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-20 17:45:41 +0200

    added openapi dependency

--- src/main/java/com/acme/autorizzazioni/licenze/LicenzaCacciaController.java
+++ src/main/java/com/acme/autorizzazioni/licenze/LicenzaCacciaController.java
@@ -1,7 +1,10 @@
 package com.acme.autorizzazioni.licenze;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,28 +23,28 @@ public class LicenzaCacciaController {
 	private LicenzaRepo licenzaRepo;
 	
 	@GetMapping("/scadute")
-	public void getTesseriniCacciaScaduti() {
+	public void getLicenzaCacciaScaduti() {
 		
 		
 		
 	}
 	
 	@GetMapping
-	public void getTesserinoCaccia() {
+	public ResponseEntity<List<LicenzaAbstract>> getLicenzaCaccia() {
 		
-		licenzaRepo.findAll();
+		return ResponseEntity.ok((List<LicenzaAbstract>) licenzaRepo.findAll());
 		
 	}
 	
 	@PostMapping
-	public void insertTesserinoCaccia( LicenzaCaccia licenzaCaccia) {
+	public void insertLicenzaCaccia( LicenzaCaccia licenzaCaccia) {
 		
 		licenzaRepo.save(licenzaCaccia);
 		
 	}
 	
 	@PutMapping("/{id}") 
-	public void updateTesserinoCaccia(@RequestBody LicenzaCaccia licenzaCaccia, @PathVariable long id) {
+	public void updateLicenzaCaccia(@RequestBody LicenzaCaccia licenzaCaccia, @PathVariable long id) {
 		
 		LicenzaCaccia licenzaCacciaDB = (LicenzaCaccia) licenzaRepo.findById(id).get(); // prendo le informazioni del tesserino al id associato
 		
@@ -52,7 +55,7 @@ public class LicenzaCacciaController {
 	}
 	
 	@DeleteMapping("/{id}")
-	public void deleteTesserinoCaccia(@PathVariable long id) {
+	public void deleteLicenzaCaccia(@PathVariable long id) {
 		
 		licenzaRepo.delete(licenzaRepo.findById(id).get());
 		
